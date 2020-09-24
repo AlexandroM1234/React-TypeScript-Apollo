0fed8b9e53968a27e0b4f7bc1c059d28b9ef2ce9
Alex Martinez <alexmartinez0071234@gmail.com>
2020-09-24 18:30:53 -0400

    updated with Apollo and migrations 47:13

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod___ } from "./constatnts";
-import { Post } from "./entities/Post";
 import mircroconfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+
 const main = async () => {
   const orm = await MikroORM.init(mircroconfig);
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
+  await orm.getMigrator().up();
+
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
