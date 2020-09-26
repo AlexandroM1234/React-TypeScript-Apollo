f4911c111fd2a9d84cf8a4faa0eab216aa2fe4e8
Alex Martinez <alexmartinez0071234@gmail.com>
2020-09-26 16:37:21 -0400

    added in more comments

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -12,13 +12,14 @@ import {
 import argon2 from "argon2";
 
 @InputType()
+// Username and password object for login validation register signup
 class UsernamePasswordIput {
   @Field()
   username: string;
   @Field()
   password: string;
 }
-
+// Error object that if an error appears will respond with what field the error was in and a message of what was wrong with it
 @ObjectType()
 class FieldError {
   @Field()
@@ -27,6 +28,7 @@ class FieldError {
   message: string;
 }
 
+// UserResponse object that returns an error if there is one and if there isn't returns the user
 @ObjectType()
 class UserResponse {
   @Field(() => [FieldError], { nullable: true })
@@ -38,12 +40,16 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  // Register Mutation returns a User type
   @Mutation(() => User)
   async register(
+    // Expects and options object that has a username and password
     @Arg("options") options: UsernamePasswordIput,
     @Ctx() { em }: MyContext
   ) {
+    // password gets hashed using argon2
     const hashedPassword = await argon2.hash(options.password);
+    // user is created in the database and returned on a successful creation
     const user = em.create(User, {
       username: options.username,
       password: hashedPassword,
@@ -52,11 +58,14 @@ export class UserResolver {
     return user;
   }
 
+  // Login mutation that can return a user if login is successful or an array of errors which have what field was wrong and a message of what was wrong with it
   @Mutation(() => UserResponse)
   async login(
+    // takes in the same options as register
     @Arg("options") options: UsernamePasswordIput,
     @Ctx() { em }: MyContext
   ): Promise<UserResponse> {
+    // if a user is not found error field is returned
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
       return {
@@ -68,12 +77,15 @@ export class UserResolver {
         ],
       };
     }
+    // Once user is found the password is validated
     const valid = await argon2.verify(user.password, options.password);
+    // if the password is invalid error is thrown
     if (!valid) {
       return {
         errors: [{ field: "password", message: "password was incorrect" }],
       };
     }
+    // if the password is correct the user is returned
     return { user };
   }
 }
